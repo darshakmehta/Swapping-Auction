807866ee987a4d160ca1690a9e4a74e651177657
tech-boy <darshak_mehta@live.com>
2018-10-25 23:23:22 -0400

    Comments added

--- controls/app.js
+++ controls/app.js
@@ -1,23 +1,32 @@
+/* Include Express */
 const express = require('express');
-
+/* Express App*/
 const app = express();
-const model = require('../models/item');
-let items = model.getItems();
 
+/* Include JavaScript Object (Model) for all Items */
+const model = require('../models/item');
+let items = model.getItems(); //Function to export all items
+
+/* Get Item by Id*/
 var getItem = (itemId) => {
 	return items[itemId];
 }
 
+/* Render views from following directory*/
 app.set('views', '../views');
+/* Set View engine*/
 app.set('view engine', 'ejs');
+/* Use static resources from following directory*/
 app.use('/resources', express.static('../resources'));
 
+/* GET Home Router */
 app.get('/', (req, res) => {
 	res.render('index', {
 		welcome: 'Not signed in.'
 	});
 });
 
+/* GET Home Router */
 app.get('/home', (req, res) => {
 	res.render('index', {
 		welcome: 'Not signed in.'
@@ -30,6 +39,7 @@ app.get('/categories', (req, res) => {
 		});
 });
 
+/* GET Sub Categories Router */
 app.get('/subCategories', (req, res) => {
 		if(req.query.catalogCategory === 'Movies' || req.query.catalogCategory === 'Vehicle') {
 			res.render('subCategories', {
@@ -44,6 +54,7 @@ app.get('/subCategories', (req, res) => {
 		}
 });
 
+/* GET Item Router */
 app.get('/item', (req, res) => {
 	if(Object.keys(req.query.length != 0)) {
 
@@ -71,18 +82,21 @@ app.get('/item', (req, res) => {
 	}
 });
 
+/* Get My Items Router*/
 app.get('/myItems', (req, res) => {
 	res.render('myItems', {
 		welcome: 'Welcome Darshak!'
 	});
 });
 
+/* Get Contact Router*/
 app.get('/contact', (req, res) => {
 	res.render('contact', {
 		welcome: 'Not signed in.'
 	});
 });
 
+/* Get About Router*/
 app.get('/about', (req, res) => {
 	res.render('about', {
 		welcome: 'Not signed in.'
@@ -95,16 +109,19 @@ app.get('/swap', (req, res) => {
 	});
 });
 
+/* Get My Swaps Router*/
 app.get('/mySwaps', (req, res) => {
 	res.render('mySwaps', {
 		welcome: 'Welcome Darshak!'
 	});
 });
 
+/* Get Any URL Router*/
 app.get('/*', (req, res) => {
 	res.send('Plain Message');
 });
 
+/* Listen Express app on Port 8080 */
 app.listen(8080, () => {
 	console.log('App listening at port 8080');
 });

e062a27164960a4736e074f5a46ab4b6fb89cff1
tech-boy <darshak_mehta@live.com>
2018-10-28 20:59:59 -0400

    User Model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+/***
+
+User {
+User ID - Unique identifier for the user
+First Name
+Last Name
+Email Address
+Address 1 Field
+Address 2 Field
+City
+State / Region
+Postal Code / Zip Code
+Country
+}
+
+***/
+const user1 = {
+	userID: '1',
+	firstName: 'Darshak',
+	lastName: 'Mehta',
+	emai: 'dmehta9@uncc.edu',
+	addressField1: '9527 University Terrace Dr.',
+	addressField2: 'Apt K',
+	city: 'Charlotte',
+	StateOrRegion: 'NC',
+	postalCode: '28262',
+	country: 'USA'
+}
+

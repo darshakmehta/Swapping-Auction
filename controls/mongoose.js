3e34970871d132e88c9687aea22ff4c5b664d596
tech-boy <darshak_mehta@live.com>
2018-11-07 23:35:41 -0500

    Mongoose setup

---
+++ controls/mongoose.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/swapAuction');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+module.exports = {
+    mongoose
+}
